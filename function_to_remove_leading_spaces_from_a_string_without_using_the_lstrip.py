88246abf37be105de5e355153d16cc14165a347c
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 17:53:02 -0800

    return the substring

--- function_to_remove_leading_spaces_from_a_string_without_using_the_lstrip.py
+++ function_to_remove_leading_spaces_from_a_string_without_using_the_lstrip.py
@@ -2,3 +2,6 @@ def custom_lstrip(input_string):
     #initialize an index to track the position of the first non-space character
     index = 0 
     while index < len(input_string) and input_string[index] == ' ': #loop through the string until we find a non space character
+        index += 1
+    #return the substring starting from the first non-space character
+    return input_string[index:]
