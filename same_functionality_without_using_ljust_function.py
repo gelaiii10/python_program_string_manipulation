4cb35bac2c408ed329e2b98dd81927c8b185b422
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 19:52:53 -0800

    added condition to check if the input is string

--- same_functionality_without_using_ljust_function.py
+++ same_functionality_without_using_ljust_function.py
@@ -4,3 +4,9 @@ input_string = "Hello"
 width = 10  
 #compute the number of spaces needed
 spaces_needed = width - len(input_string)
+#if the input string is already longer than the specified width return it as is
+if spaces_needed <= 0:
+    result = input_string
+else:
+    #add spaces to the end of the string
+    result = input_string + ' ' * spaces_needed
