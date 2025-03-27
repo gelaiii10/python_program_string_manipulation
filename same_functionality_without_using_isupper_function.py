79c185d6a026387ef862e749b75c767081ee325c
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 19:19:47 -0800

    use for loop to check if the character is not an uppercase

--- same_functionality_without_using_isupper_function.py
+++ same_functionality_without_using_isupper_function.py
@@ -1,3 +1,9 @@
 #input a string
-input_string = "Hello, World!"
-result = ""
+input_string = "HELLO WORLD!"  #change this to test with different strings
+all_upper = True  #assume the string is all uppercase initially
+
+for i in input_string:
+    #check if the character is not an uppercase letter
+    if not ('A' <= i <= 'Z'):
+        all_upper = False  #found a non-uppercase character
+        break  #break the code
