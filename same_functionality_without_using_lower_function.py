7fc4906f7c0113fc8737c0af3c88ee83b552044d
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 19:06:07 -0800

    use for loop to check if the character is an uppercase

--- same_functionality_without_using_lower_function.py
+++ same_functionality_without_using_lower_function.py
@@ -1,3 +1,11 @@
 #input a string
 input_string = "Hello, World!"
 result = ""
+
+for i in input_string:
+    #check if the character is an uppercase letter
+    if 'A' <= i <= 'Z':
+        result += chr(ord(i) + 32) #convert to lowercase 
+    else:
+        #if it's not an uppercase letter keep it as is
+        result += i
