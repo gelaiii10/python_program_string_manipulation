7911854a278954a7b51a94321d0534077d2b9c5c
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 20:40:07 -0800

    added for loop and condition to check the characters

--- same_functionality_without_using_title_function.py
+++ same_functionality_without_using_title_function.py
@@ -1,3 +1,13 @@
 input_string = "hello, world! this is just a test."  #original string
 result = ""
 capitalize_next = True  #flag to indicate if the next character should be capitalized
+
+for i in input_string:
+    if i == ' ':
+        result += i  #keep spaces as they are
+        capitalize_next = True  #next character should be capitalized
+    elif capitalize_next:
+        result += i.upper()  #capitalize the character
+        capitalize_next = False  #reset the flag
+    else:
+        result += i.lower()  #make the character lowercase
