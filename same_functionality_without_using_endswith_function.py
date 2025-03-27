7085219d74e7ccb52743381a953cb57fdaf3fd79
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 19:36:02 -0800

    print the result

--- same_functionality_without_using_endswith_function.py
+++ same_functionality_without_using_endswith_function.py
@@ -5,3 +5,8 @@ if len(suffix) > len(input_string):
     ends_with_suffix = False
 else:
     ends_with_suffix = input_string[-len(suffix):] == suffix #compare the end of the string with the suffix
+
+if ends_with_suffix: #print the result
+    print(f"the string '{input_string}' ends with '{suffix}'.")
+else:
+    print(f"the string '{input_string}' does not end with '{suffix}'.")   
