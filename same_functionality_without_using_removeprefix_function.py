480efada26ef1ed395128bf9ff90c70d07c45e62
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 18:37:34 -0800

    print the result

--- same_functionality_without_using_removeprefix_function.py
+++ same_functionality_without_using_removeprefix_function.py
@@ -3,3 +3,8 @@ def custom_removeprefix(input_string, prefix):
     if input_string.startswith(prefix):
         return input_string[len(prefix):]
     return input_string  #return the original string
+
+input_str = "Hello, World!" 
+prefix = "Hello, "
+result = custom_removeprefix(input_str, prefix)
+print(result) #print the result
