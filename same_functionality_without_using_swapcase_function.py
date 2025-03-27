504bafac4c6980d6866c7a2a90997e736f2bef83
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 20:27:20 -0800

    used for loop to iterates through each character in input string

--- same_functionality_without_using_swapcase_function.py
+++ same_functionality_without_using_swapcase_function.py
@@ -1,2 +1,14 @@
 input_string = "Hello, World!"  #original string
 result = ""
+
+for i in input_string:
+    #check if the character is uppercase
+    if 'A' <= i <= 'Z':
+        #convert to lowercase by adding 32
+        result += chr(ord(i) + 32)
+    #check if the character is lowercase
+    elif 'a' <= i <= 'z':
+        #convert to uppercase by subtracting 32
+        result += chr(ord(i) - 32)
+    else:
+        result += i
