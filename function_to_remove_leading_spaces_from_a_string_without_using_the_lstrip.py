ed699bb0ac4570ff6a86814fe7bac3c159dea603
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 17:51:25 -0800

    added while loop through the string until i find a non space character

--- function_to_remove_leading_spaces_from_a_string_without_using_the_lstrip.py
+++ function_to_remove_leading_spaces_from_a_string_without_using_the_lstrip.py
@@ -1,3 +1,4 @@
 def custom_lstrip(input_string):
     #initialize an index to track the position of the first non-space character
     index = 0 
+    while index < len(input_string) and input_string[index] == ' ': #loop through the string until we find a non space character
