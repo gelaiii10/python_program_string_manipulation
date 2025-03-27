5eaed5b130e6a763a3387cbdd1268bc7c15f0928
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 19:32:19 -0800

    check if the length of the suffix is greater than the string

--- same_functionality_without_using_endswith_function.py
+++ same_functionality_without_using_endswith_function.py
@@ -1,2 +1,7 @@
 input_string = "Hello, World!"  #string to check
 suffix = "World!"                #suffix to check
+#check if the length of the suffix is greater than the string
+if len(suffix) > len(input_string):
+    ends_with_suffix = False
+else:
+    ends_with_suffix = input_string[-len(suffix):] == suffix #compare the end of the string with the suffix
