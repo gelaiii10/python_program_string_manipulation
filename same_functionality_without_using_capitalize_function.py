51a1efcca00e5714e0024cfebc1ef054e708b955
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-27 12:50:36 -0800

    added condition to check if the string is empty

--- same_functionality_without_using_capitalize_function.py
+++ same_functionality_without_using_capitalize_function.py
@@ -1,2 +1,9 @@
 #input string
 input_string = "hELLO wORLD"
+#check if the string is empty
+if not input_string:
+    result = input_string
+else:
+    #capitalize the first character and make the rest lowercase
+    first_char = input_string[0].upper()  #capitalize the first character
+    rest_of_string = input_string[1:].lower()  #convert the rest to lowercase
