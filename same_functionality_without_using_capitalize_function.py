eeb1d397b826c7b473f26a0e51b2f431f218d45c
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-27 12:53:18 -0800

    connect the results

--- same_functionality_without_using_capitalize_function.py
+++ same_functionality_without_using_capitalize_function.py
@@ -7,3 +7,5 @@ else:
     #capitalize the first character and make the rest lowercase
     first_char = input_string[0].upper()  #capitalize the first character
     rest_of_string = input_string[1:].lower()  #convert the rest to lowercase
+
+    result = first_char + rest_of_string #integrate the results
