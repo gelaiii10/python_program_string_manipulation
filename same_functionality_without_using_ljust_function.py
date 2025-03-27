5a9090b04076bb37e2f07b022fe4db40e55af2f4
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-26 19:47:12 -0800

    compute the number of spaces

--- same_functionality_without_using_ljust_function.py
+++ same_functionality_without_using_ljust_function.py
@@ -1,4 +1,6 @@
 #original string
 input_string = "Hello"
 #desired total width
-width = 10              
+width = 10  
+#compute the number of spaces needed
+spaces_needed = width - len(input_string)
