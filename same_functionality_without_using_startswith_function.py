982d9ce8c4c8f9e2d7cacdd7c138de8b1eb230af
gelaiii10 <gelaibacleaan@gmail.com>
2025-04-01 11:34:32 -0800

    added condition to check is the prefix is longer than the string

--- same_functionality_without_using_startswith_function.py
+++ same_functionality_without_using_startswith_function.py
@@ -1,3 +1,8 @@
 #input string and prefix
 string = "hello world"
 prefix = "hello"
+#check if the prefix is longer than the string
+if len(prefix) > len(string):
+    result = False
+else:
+    result = True #initialize a flag to true
