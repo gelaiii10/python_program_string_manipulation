ddb1cddb525e9f42a2ca4bd9edc4442033adbc18
gelaiii10 <gelaibacleaan@gmail.com>
2025-04-01 11:39:25 -0800

    used for loop to compare the beginning of the string with prefix

--- same_functionality_without_using_startswith_function.py
+++ same_functionality_without_using_startswith_function.py
@@ -6,3 +6,8 @@ if len(prefix) > len(string):
     result = False
 else:
     result = True #initialize a flag to true
+    #compare the beginning of the string with the prefix
+    for i in range(len(prefix)):
+        if string[i] != prefix[i]:
+            result = False
+            break
