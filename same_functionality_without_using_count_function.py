f3d88275f17c632b15a790ba6f8fcf89bac54016
gelaiii10 <gelaibacleaan@gmail.com>
2025-04-01 12:29:56 -0800

    used for loop

--- same_functionality_without_using_count_function.py
+++ same_functionality_without_using_count_function.py
@@ -4,3 +4,8 @@ substring = "hello"
 #initialize a counter
 count = 0
 substring_length = len(substring)
+#for loop through the string to find occurrences of the substring
+for i in range(len(string) - substring_length + 1):
+    #check if the substring matches the part of the string
+    if string[i:i + substring_length] == substring:
+        count += 1
