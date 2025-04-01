42d38d31e183b710a664cc96e170f4f43fdcdd3b
gelaiii10 <gelaibacleaan@gmail.com>
2025-04-01 12:04:58 -0800

    used for loop to check each character in the string

--- same_functionality_without_using_islower_function.py
+++ same_functionality_without_using_islower_function.py
@@ -2,3 +2,9 @@
 string = "hello world"
 #initialize a flag to True
 all_lower = True
+#check each character in the string
+for i in string:
+    #if the character is not a lowercase letter
+    if not ('a' <= i <= 'z'):
+        all_lower = False
+        break #break the code
