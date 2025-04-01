6814996a655ead0a0e797509dfd79e7ea8635edb
gelaiii10 <gelaibacleaan@gmail.com>
2025-04-01 11:56:05 -0800

    added condition

--- same_functionality_without_using_rjust_function.py
+++ same_functionality_without_using_rjust_function.py
@@ -3,3 +3,9 @@ string = "HELLO"
 width = 10
 #calculate the number of spaces needed
 spaces_needed = width - len(string)
+#if spaces_needed is less than or equal to 0, use the original string
+if spaces_needed <= 0:
+    result = string
+else:
+    #create a string of spaces and connect it with the original string
+    result = ' ' * spaces_needed + string
