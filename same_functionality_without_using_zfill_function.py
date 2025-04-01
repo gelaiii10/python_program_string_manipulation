18be65a61a00efe3ce30f219f1dbddd930592ff3
gelaiii10 <gelaibacleaan@gmail.com>
2025-04-01 12:18:57 -0800

    added condition to check if the variable is less than or equal to zero

--- same_functionality_without_using_zfill_function.py
+++ same_functionality_without_using_zfill_function.py
@@ -3,3 +3,9 @@ string = "42"
 width = 5
 #calculate the number of zeros needed
 zeros_needed = width - len(string)
+#if zeros_needed is less than or equal to 0 use the original string
+if zeros_needed <= 0:
+    result = string
+else:
+    #create a string of zeros and connect it with the original string
+    result = '0' * zeros_needed + string
