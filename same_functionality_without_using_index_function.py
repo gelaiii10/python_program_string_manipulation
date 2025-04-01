f03ad34aa50f75096a7264d5db7f5e3554f00a23
gelaiii10 <gelaibacleaan@gmail.com>
2025-04-01 12:37:31 -0800

    initialize a variable to store the index

--- same_functionality_without_using_index_function.py
+++ same_functionality_without_using_index_function.py
@@ -1,3 +1,6 @@
 #input string and substring to find
 string = "hello philippines, hello world"
 substring = "world"
+#initialize a variable to store the index
+index = -1
+substring_length = len(substring)
