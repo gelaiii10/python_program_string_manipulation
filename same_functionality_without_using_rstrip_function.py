b3547b24a3cedc23250a631317de51cd325514eb
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-31 21:01:00 -0800

    get the substring from the start and print the result

--- same_functionality_without_using_rstrip_function.py
+++ same_functionality_without_using_rstrip_function.py
@@ -2,5 +2,10 @@
 input_string = "hello, world     " #this is just example of string
 index = len(input_string) - 1  #start from the end of the string and find the last non space character
 
-while index >= 0 and input_string[index].isspace()
+while index >= 0 and input_string[index].isspace():
     index -= 1
+#get the substring from the start to the last non space character
+result = input_string[:index + 1]
+#print the result
+print(input_string)
+print(result)
