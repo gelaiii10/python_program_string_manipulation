8eefe0de1a15aa4826624574ef8c7f1a2104ed43
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-31 20:56:57 -0800

    added while loop to continues as long as two conditions are true

--- same_functionality_without_using_rstrip_function.py
+++ same_functionality_without_using_rstrip_function.py
@@ -1,2 +1,6 @@
 #input string
 input_string = "hello, world     " #this is just example of string
+index = len(input_string) - 1  #start from the end of the string and find the last non space character
+
+while index >= 0 and input_string[index].isspace()
+    index -= 1
