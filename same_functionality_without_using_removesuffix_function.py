1e2f3cc9047523882d5c4952ceb985ebf8525a61
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-31 21:20:11 -0800

    print the result

--- same_functionality_without_using_removesuffix_function.py
+++ same_functionality_without_using_removesuffix_function.py
@@ -6,3 +6,8 @@ if input_string.endswith(suffix):
     new_length = len(input_string) - len(suffix) #calculate the new length of the string without the suffix
     #slit the string to remove the suffix
     result = input_string[:new_length]
+else:
+    result = input_string
+#print the result
+print(input_string)
+print(result)    
