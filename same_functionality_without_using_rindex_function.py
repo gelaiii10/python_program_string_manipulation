c672b7239f4fd8e1ce7f576f2fee1d1e2adb91fe
gelaiii10 <gelaibacleaan@gmail.com>
2025-04-01 12:52:30 -0800

    added for loop

--- same_functionality_without_using_rindex_function.py
+++ same_functionality_without_using_rindex_function.py
@@ -4,3 +4,10 @@ substring = "hello"
 #initialize a variable to store the index
 index = -1
 substring_length = len(substring)
+#for L
+# loop through the string in reverse to find the last occurrence of the substring
+for i in range(len(string) - substring_length, -1, -1):
+    #check if the substring matches the part of the string
+    if string[i:i + substring_length] == substring:
+        index = i
+        break  #exit the loop once the last occurrence is found
