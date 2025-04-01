3dc4f93dace99f5fc9ae59056a8bc89fd989c0c8
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-31 21:33:47 -0800

    used for loop to repeaf through each character in input string

--- same_functionality_without_using_upper_function.py
+++ same_functionality_without_using_upper_function.py
@@ -1,3 +1,9 @@
 #input string
 input_string = "hello world!"
 result = "" #initialize an empty string to store the result
+#repeat through each character in the input string
+for i in input_string:
+    if 'a' <= i <= 'z': #check if the character is a lowercaze letter
+        result += chr(ord(i) - 32) #convert to uppercase by subtracting
+else:
+    result += i        
